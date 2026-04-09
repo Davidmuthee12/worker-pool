02c3d6ff894aed5e56980a06168b20c3c82dc903
unknown <mutheedavid12@gmail.com>
2026-04-09 12:32:39 +0300

    feat: adds full implementation for image/files worker-pool processer

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, jobs <-chan string, results chan<- string) {
+	for file := range jobs {
+		fmt.Printf("Worker %d started processing %s\n", id, file)
+
+		time.Sleep(2 * time.Second)
+
+		fmt.Printf("Worker %d finished processing %s\n", id, file)
+
+		results <- fmt.Sprintf("%s processed by worker %d", file, id)
+	}
+}
+
 
 func main() {
-	fmt.Println("Worker Pool Example")
+	files := []string {
+		"image1.jpg",
+		"image2.png",
+		"document.pdf",
+		"video.mp4",
+		"notes.txt",
+	}
+
+	jobs := make(chan string, len(files))
+	results := make(chan string, len(files))
+
+	// Here we create the workers
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Here we send the jobs to the workers
+	for _, file := range files {
+		jobs <- file
+	}
+
+	close(jobs)
+
+	// Here we read the results
+	for a := 1; a <= len(files); a++ {
+		fmt.Println(<-results)
+	}
 }
